26185d36f303631dd7e5497188ac7902d719b43b
vikrugpro <vikrugpro@gmail.com>
2024-05-19 21:50:14 +0300

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chart } from 'react-google-charts';
+const options = {
+    title: 'Любимые занятия котика Конфуция в течение дня',
+};
+const data = [
+                    ['Занятие', 'количество часов в день'],
+                    ['Кушать', 2],
+                    ['Гладить пузико',1],
+                    ['Мяукать', 1],
+                    ['Сладенько спать', 18],
+                    ['Укоризненно смотреть на рабов', 2],
+                ];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Chart
+                chartType="PieChart"
+                options={options}
+                data={data}
+                width="100%"
+                height="400px"
+            />
+        </div>
+    );
 }
 
 export default App;
